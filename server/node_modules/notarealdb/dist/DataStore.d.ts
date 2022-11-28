f52e58f13fb43fae55f61cd9fd676afb85667b4a
1Aboka1 <zz.abulhair.zz@gmail.com>
2022-11-28 18:42:00 +0600

    delete node_modules

--- server/node_modules/notarealdb/dist/DataStore.d.ts
+++
@@ -1,7 +0,0 @@
-import { Collection } from './Collection';
-import { Entity } from './Entity';
-export declare class DataStore {
-    private dir;
-    constructor(dir: string);
-    collection<T extends Entity>(name: string): Collection<T>;
-}
--- server/node_modules/notarealdb/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './Entity';
-export * from './Collection';
-export * from './DataStore';
